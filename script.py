e2e9210bf81c57ceb0eda267abbbf6122fb18eb2
mathilda0902 <mathilda_sorciere@yahoo.com>
2021-04-09 00:41:10 -0700

    added partition

--- script.py
+++ script.py
@@ -1,34 +1,39 @@
 from pyspark import SparkContext
 from pyspark.sql import functions as F
-from pyspark.sql.types import ArrayType, IntegerType, StructType, StructField, StringType, FloatType
+from pyspark.sql import Row
+from pyspark.sql.functions import udf, struct
+from pyspark.sql.types import ArrayType, IntegerType, StructType, StructField, StringType, DoubleType
 import json
 
-
-def parse_json(array_str):
-    json_obj = json.loads(array_str)
-    return json_obj
-
-def construct_json_array():
-    row_array = []
-    for row in text_file.take(10):
-        row_array.append(parse_json(row))
+def construct_json_rdd(text_file):
+    rdd = text_file.map(lambda r: json.loads(r))
+    return rdd
 
 def construct_schema():
-    json_schema = StructType([StructField('batch_id', StringType(), nullable=False), 
+    schema = StructType([StructField('batch_id', StringType(), nullable=False), 
                     StructField('vendor_id', StringType(), nullable=False),
                     StructField('product_id', StringType(), nullable=False),
                     StructField('lab_id', StringType(), nullable=False),
                     StructField('state', StringType(), nullable=False),
                     StructField('tested_at', StringType(), nullable=False),
                     StructField('expires_at', StringType(), nullable=False),
-                    StructField('thc', FloatType(), nullable=False),
-                    StructField('thca', FloatType(), nullable=False),
-                    StructField('cbd', FloatType(), nullable=False),
-                    StructField('cbda', FloatType(), nullable=False)])
-    return json_schema
+                    StructField('thc', DoubleType(), nullable=False),
+                    StructField('thca', DoubleType(), nullable=False),
+                    StructField('cbd', DoubleType(), nullable=False),
+                    StructField('cbda', DoubleType(), nullable=False)])
+    return schema
 
-text_file = sc.textFile('s3://emr-spark-notebook-data-challenge/file1.txt.gz')
+def main():
+    sc = SparkContext()
+    text_file = sc.textFile('s3://emr-spark-notebook-data-challenge/file1.txt.gz')
+    data_rdd = construct_json_rdd(text_file)
+    schema = construct_schema()
+    df = sc.createDataFrame(data=data_rdd,schema=schema)
+    # n = 12 
+    # spark_df = df2.repartition(n)
+    spark_df = df.limit(1000)
+    spark_df.write.partitionBy("tested_at").parquet("s3a://data-challenge-lab-tests/",mode="append")
 
-df = spark.createDataFrame(data=row_array,schema=json_schema)
-df2 = df.withColumn("tested_at_date", F.col("tested_at").cast("date"))
-df2 = df2.withColumn("expires_at_date", F.col("expires_at").cast("date"))
+
+if __name__ == '__main__':
+    main()
